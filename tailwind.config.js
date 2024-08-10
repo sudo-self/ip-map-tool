a844b3aa30a1c1283008188c64a9284a1d9428ca
Sudo-Self <sudo-self@proton.me>
2024-08-09 23:20:07 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      },
+    },
+  },
+  plugins: [],
+}
+
